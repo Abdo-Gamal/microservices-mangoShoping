afa19b441aefffd690ec4ae51c543ca6e837da52
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-11-29 21:20:22 +0200

    add mappingConfige

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+           // CreateMap<Product, ProductDto>();
+
+           // CreateMap<ProductDto, Product>();
+            //  CreateMap<invoice, invoicesModelView>()
+            //.ForSourceMember(source => source.Total, opt => opt.DoNotValidate())
+            //.ReverseMap();
+
+        }
+    }
+}
