c41e8956283f209e05021dbcfe05ca6cfc2a9d20
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-11-30 00:08:46 +0200

    magration

--- Mango.Services.ShoppingCartAPI/Migrations/20210504202216_AddProductAndCartModels.Designer.cs
+++ Mango.Services.ShoppingCartAPI/Migrations/20210504202216_AddProductAndCartModels.Designer.cs
@@ -3,14 +3,16 @@ using Mango.Services.ShoppingCartAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Mango.Services.ShoppingCartAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210504202216_AddProductAndCartModels")]
+    partial class AddProductAndCartModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
