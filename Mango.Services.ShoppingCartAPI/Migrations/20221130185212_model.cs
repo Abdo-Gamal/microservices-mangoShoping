133e397575053bcec8961d7f9c1005ecf190e81a
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-11-30 21:20:19 +0200

    solve  magration error

---
+++ Mango.Services.ShoppingCartAPI/Migrations/20221130185212_model.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ShoppingCartAPI.Migrations
+{
+    public partial class model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
