8581c595a37357843d0a85eb8d80bb810fc53311
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-12-01 02:41:15 +0200

    make CartRepositray

---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public CartRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
