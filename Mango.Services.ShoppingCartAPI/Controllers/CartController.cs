c4de450be1265f68f1b80078427decbcb769bb49
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-12-02 21:44:48 +0200

    add controller

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
