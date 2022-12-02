9bf87b54fda8e683084f9710df61a3a320990e38
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-12-02 14:19:34 +0200

    make  create or update method in  shopingCart repositray

--- Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Repository
 {
@@ -14,32 +16,85 @@ namespace Mango.Services.ShoppingCartAPI.Repository
             _db = db;
             _mapper = mapper;
         }
-        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        public  async Task<bool> ApplyCoupon(string userId, string couponCode)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> ClearCart(string userId)
+        public async Task<bool> ClearCart(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            Cart cart = _mapper.Map<Cart>(cartDto);
+
+            //check if product  exist in DB ,if  not create it .
+            //in front  in each time we add one product so  we  write first or default .
+            var prodInDb = _db.Products.FirstOrDefaultAsync(
+                u=>u.ProductId==cartDto.CartDetails.FirstOrDefault().ProductId);
+            if (prodInDb == null)
+            {
+                _db.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+                await _db.SaveChangesAsync();
+            }
+            //check if header is null 
+            var CartHeaderFromDb = await _db.CartHeaders.AsNoTracking().FirstOrDefaultAsync(
+                u=>u.CartHeaderId==cart.CartHeader.CartHeaderId);
+            if (CartHeaderFromDb == null)
+            {
+                //cread header and details 
+                _db.CartHeaders.Add(cart.CartHeader);
+                await _db.SaveChangesAsync();
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+                // in cartdetails  exist product object so ef try to add product with same id
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                //if header not null 
+                // check if details  has same product
+                var CartDetailFromDb =await _db.CartDetails.AsNoTracking().FirstOrDefaultAsync(
+                    u => u.ProductId == cart.CartDetails.FirstOrDefault().ProductId &&
+                    u.CartHeaderId == CartHeaderFromDb.CartHeaderId
+                    ) ;
+
+                if (CartDetailFromDb == null)
+                {
+                    //crate cartDetails 
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = CartHeaderFromDb.CartHeaderId;
+                    // in cartdetails  exist product object so ef try to add product with same id
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    //update the count / cart details
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    cart.CartDetails.FirstOrDefault().Count+= CartDetailFromDb.Count;
+                    _db.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+
+                }
+            }
+            return  _mapper.Map<CartDto>(cart);
+        }
+
+        public async Task<CartDto> GetCartByUserId(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CartDto> GetCartByUserId(string userId)
+        public async Task<bool> RemoveCoupon(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> RemoveCoupon(string userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> RemoveFromCart(int cartDetailsId)
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
         {
             throw new NotImplementedException();
         }
