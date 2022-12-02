46c9a552a8d4af6e33e7fe05336e7d9f9d588f79
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-12-02 23:54:31 +0200

    make  Icartservices

---
+++ Mango.web/Services/IServices/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using Mango.web.Models;
+
+namespace Mango.web.Services.IServices
+{
+    public interface ICartService
+    {
+
+        Task<T> GetCartByUserIdAsnyc<T>(string userId,string token=null);
+        Task<T> AddToCartAsnyc<T>(CartDto cartDto, string token = null);
+
+        Task<T> UpdateCartAsnyc<T>(CartDto cartDto, string token = null);
+        //I mean by cartId  is cartDetailsId
+        Task<T> RemoveFromCartAsnyc<T>(int CartId, string token = null);
+
+    }
+}
