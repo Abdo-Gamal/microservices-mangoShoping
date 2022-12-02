8efc2a186dd4e0351e3eeef32b2f3f8e260be5aa
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-12-02 22:45:16 +0200

    add OrdarTotal in  CartHeaderDto

--- Mango.web/Models/CartHeaderDto.cs
+++ Mango.web/Models/CartHeaderDto.cs
@@ -5,5 +5,7 @@
         public int CartHeaderId { get; set; }
         public string UserId { get; set; }
         public string CouponCode { get; set; }
+        // to make some calculation in  only mango.web
+        public double OrdarTotal { get; set; }
     }
 }
