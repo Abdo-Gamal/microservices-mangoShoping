eb52c362d36cc7731fdcf2878840635b8c329465
abdo-gamal <abdulrahmangamalahmedgaber@gmail.com>
2022-12-03 03:37:42 +0200

    modify removeCar in controller

--- Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -11,8 +11,8 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
     public class CartController : Controller
     {
         private readonly ICartRepository _cartRepository;
-        protected  ResponseDto _response;
-        public  CartController(ICartRepository cartRepository)
+        protected ResponseDto _response;
+        public CartController(ICartRepository cartRepository)
         {
             _cartRepository = cartRepository;
             _response = new ResponseDto();
@@ -64,20 +64,17 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             return _response;
         }
         [HttpPost("RemoveCart")]
-        public async Task<object> RemoveCart([FromBody]int  CartId)
+        public async Task<object> RemoveCart([FromBody] int cartId)
         {
             try
             {
-                bool  isSuccess = await _cartRepository.RemoveFromCart(CartId);
+                bool isSuccess = await _cartRepository.RemoveFromCart(cartId);
                 _response.Result = isSuccess;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
                 _response.IsSuccess = false;
-                _response.ErrorMessages = new List<string>()
-                {
-                    e.ToString()
-                };
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
             return _response;
         }
